7a531e6c99e0d123aac07ef8f8887decdfbe3607
Julian Caicedo <julian.caicedo@tekus.co>
2018-01-06 16:18:05 +0100

    Initial import

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '11.2'
+use_frameworks!
+
+def common
+	pod 'SwiftLint' 
+    pod 'RxSwift', '~> 4.1'
+    pod 'RxCocoa', '~> 4.1'
+end
+
+target 'RxSwiftBook' do
+  common
+end
---
+++ RxSwiftBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwiftBook
+//
+//  Created by Julian Caicedo on 06.01.18.
+//  Copyright © 2018 Julian Caicedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
